86df2f44ea3369e36981a6751f778fea516b13a6
pino <0701pino@kumonekodo.com>
2023-08-25 09:18:41 +0000

    aタグをLinkコンポーネントに修正

--- next_app/src/components/post-list.tsx
+++ next_app/src/components/post-list.tsx
@@ -1,6 +1,7 @@
 import { PostData } from "@/types/post-data";
 import { MdAccessTime, MdUpdate } from "react-icons/md";
 import Image from "next/image";
+import Link from "next/link";
 import { formatDate } from "@/lib/util";
 import { getImageAttributes } from "@/lib/get-contents";
 
@@ -15,50 +16,53 @@ const PostList: React.FC<PostListProps> = ({ posts: posts }) => {
         const { imageSrc, imageSize } = getImageAttributes(item.emoji, item.image);
 
         return (
-          <a
-            key={item.slug}
-            href={"/post/" + item.slug}
-            className="flex flex-col rounded-lg bg-white overflow-hidden hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-indigo-600"
-            style={{ height: "auto" }}
-          >
-            <div className="flex flex-col-reverse" style={{ height: "auto" }}>
-              <div className="flex-1">
-                <div className="p-3">
-                  <div className="flex space-x-4 items-center">
-                    <div className="flex items-center space-x-2">
-                      <MdAccessTime />
-                      <p className="text-sm text-gray-600">{formatDate(item.createdAt)}</p>
-                    </div>
-                    {item.updatedAt && item.createdAt !== item.updatedAt && (
-                      <div className="flex items-center space-x-2">
-                        <MdUpdate />
-                        <p className="text-sm text-gray-600">{formatDate(item.updatedAt)}</p>
+          <>
+            <Link href={"/post/" + item.slug} passHref>
+              <div
+                key={item.slug}
+                className="flex flex-col rounded-lg bg-white overflow-hidden hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-indigo-600 cursor-pointer"
+                style={{ height: "auto" }}
+              >
+                <div className="flex flex-col-reverse" style={{ height: "auto" }}>
+                  <div className="flex-1">
+                    <div className="p-3">
+                      <div className="flex space-x-4 items-center">
+                        <div className="flex items-center space-x-2">
+                          <MdAccessTime />
+                          <p className="text-sm text-gray-600">{formatDate(item.createdAt)}</p>
+                        </div>
+                        {item.updatedAt && item.createdAt !== item.updatedAt && (
+                          <div className="flex items-center space-x-2">
+                            <MdUpdate />
+                            <p className="text-sm text-gray-600">{formatDate(item.updatedAt)}</p>
+                          </div>
+                        )}
                       </div>
-                    )}
+                      <h3 className="text-xl font-bold text-gray-900">{item.title}</h3>
+                      <p className="mt-3 text-base text-gray-600">
+                        {item.description && item.description?.length > 50
+                          ? `${item.description?.substring(0, 50)}...`
+                          : item.description}
+                      </p>
+                    </div>
                   </div>
-                  <h3 className="text-xl font-bold text-gray-900">{item.title}</h3>
-                  <p className="mt-3 text-base text-gray-600">
-                    {item.description && item.description?.length > 50
-                      ? `${item.description?.substring(0, 50)}...`
-                      : item.description}
-                  </p>
+                  <div className="relative w-full flex justify-center">
+                    <Image
+                      src={imageSrc}
+                      alt=""
+                      className="object-cover w-full"
+                      width={1200}
+                      height={630}
+                      style={{
+                        width: `${imageSize}`,
+                        height: "auto",
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="relative w-full flex justify-center">
-                <Image
-                  src={imageSrc}
-                  alt=""
-                  className="object-cover w-full"
-                  width={1200}
-                  height={630}
-                  style={{
-                    width: `${imageSize}`,
-                    height: "auto",
-                  }}
-                />
-              </div>
-            </div>
-          </a>
+            </Link>
+          </>
         );
       })}
     </div>
