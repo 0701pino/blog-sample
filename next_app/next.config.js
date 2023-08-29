686a9c7ffd6785f5951fa56e171760348e0367e5
pino <0701pino@kumonekodo.com>
2023-08-29 09:26:10 +0000

    trailingSlashを有効に設定

--- next_app/next.config.js
+++ next_app/next.config.js
@@ -6,6 +6,7 @@ const isProd = process.env.NODE_ENV == "production";
 
 const nextConfig = withExportImages({
   output: "export",
+  trailingSlash: true,
   basePath: isProd ? SUB_DIRECTORY : "",
 });
 
